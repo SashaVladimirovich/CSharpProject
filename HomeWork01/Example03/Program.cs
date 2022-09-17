c31b425fd8e7151cc1ce2abd35b89c95051c11a8
Sahsa <a.v.pavlov09@gmail.com>
2022-09-17 13:25:53 +0300

    Решение третьей задачи

---
+++ HomeWork01/Example03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите число: ");
+int a = Convert.ToInt16(Console.ReadLine());
+if(a % 2 == 0)
+{
+    Console.Write(a);
+    Console.Write(" - чётное число");
+}
+else
+{
+    Console.Write(a);
+    Console.Write(" - нечётное число"); 
+}
