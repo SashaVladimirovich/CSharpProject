71a1ee682064f80b633092be49657d23cd50990e
Sahsa <a.v.pavlov09@gmail.com>
2022-09-17 13:21:41 +0300

    Решение второй задачи

---
+++ HomeWork01/Example02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Ведите число 1: ");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.Write("Ведите число 2: ");
+int b = Convert.ToInt16(Console.ReadLine());
+Console.Write("Ведите число 3: ");
+int c = Convert.ToInt16(Console.ReadLine());
+int max = a;
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+Console.Write("Максимальное число ");
+Console.Write(max);
