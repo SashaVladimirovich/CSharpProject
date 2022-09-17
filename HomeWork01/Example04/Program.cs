e1ac831a88687db3420a68e4b6b857f2dac2469b
Sahsa <a.v.pavlov09@gmail.com>
2022-09-17 13:30:15 +0300

    Решение четвёртой задачи

---
+++ HomeWork01/Example04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Ведите число: ");
+int N = Convert.ToInt16(Console.ReadLine());
+for (int i = 2; i < N+1; i++)       
+if (i%2==0) 
+{           
+Console.Write(i);
+Console.Write(". ");
+}
