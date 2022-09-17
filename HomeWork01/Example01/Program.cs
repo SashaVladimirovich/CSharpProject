209199c219d8bbcd5586f95d8c4c458e1abcec36
Sahsa <a.v.pavlov09@gmail.com>
2022-09-17 13:17:24 +0300

    Решение первой задачи

---
+++ HomeWork01/Example01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Ведите число 1: ");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.Write("Ведите число 2: ");
+int b = Convert.ToInt16(Console.ReadLine());
+if(a > b)
+{
+    Console.Write(a);
+    Console.Write(" больше ");
+    Console.Write(b);
+}
+else
+{
+    Console.Write(b);
+    Console.Write(" больше ");
+    Console.Write(a);
+}
