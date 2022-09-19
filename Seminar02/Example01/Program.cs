ef4e7ca0bd040ccf78630e46d78a80a2dfccbf05
Sahsa <a.v.pavlov09@gmail.com>
2022-09-19 21:21:02 +0300

    Решение задачи 1

---
+++ Seminar02/Example01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Шалом, Юзер");
+Console.Write("Загадано число: ");
+
+
+int n = new Random().Next(10,100);
+int a = n / 10;
+int b = n % 10;
+
+System.Console.WriteLine(n);
+Console.Write("Максимальная цифра: ");
+if(a > b) 
+{
+    Console.Write(a);
+}
+else
+{
+    Console.Write(b);
+}
