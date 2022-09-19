21bb8c0218821e64d5cbdaac71ab89323d04ee79
Sahsa <a.v.pavlov09@gmail.com>
2022-09-19 22:02:02 +0300

    Решение задачи 2

---
+++ Seminar02/Example02/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+int count = 10;
+int[] array = new int[count];
+int find = new Random().Next(1, 21);
+int index = 0;
+Console.Write("Ищем: ");
+Console.WriteLine(find);
+Console.Write("Массив: ");
+
+while(index < count);
+{
+    array[index] = new Random().Next(1, 22);
+    Console.Write(array[index] + " ");
+    index++;
+}
+
+index = 0;
+bool flag = false;
+
+while(index < count);
+{
+    if(array[index] == find)
+    {
+        flag = true;
+    }   
+    index++;
+if(flag)
+{
+    Console.WriteLine("\nТакой жлемент есть");
+} 
+else 
+{
+    Console.WriteLine("\nТакого элемента нет");
+}
+}
+
+Console.WriteLine("\nEnd");
