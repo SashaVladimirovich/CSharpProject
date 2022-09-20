d193356bcb5d9ae9673b5f213a3596eb8620e19d
Sahsa <a.v.pavlov09@gmail.com>
2022-09-20 19:36:06 +0300

    Решение Задачи №15

---
+++ HomeWork02/Example03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Здравствуйте. Введите порядковый номер дня недели: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (N < 1 || N > 7)
+{
+    Console.WriteLine("Ошибка!!! Дня недели с таким порядковым номером не существует!!!");
+    Console.Write("Введите порядковый номер дня недели: ");
+    N = Convert.ToInt32(Console.ReadLine());
+}
+if(N == 6 || N == 7)
+{
+    Console.Write("Выходной день");
+}
+else
+{
+    Console.Write("Будни");
+}
