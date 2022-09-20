05d78870426fdf17eb167c5a7b889797ff4ffa2f
Sahsa <a.v.pavlov09@gmail.com>
2022-09-20 16:09:43 +0300

    Решение Задачи №10

---
+++ HomeWork02/Example01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Здравствуйте. Введите положительное трёхзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (N < 100 || N > 999)
+{
+    Console.WriteLine("Ошибка!!!");
+    Console.Write("Введите положительное трёхзначное число: ");
+    N = Convert.ToInt32(Console.ReadLine());
+}
+
+int a = N % 100;
+int b = a / 10;
+
+Console.Write("Вторая цифра заданного числа: " + b + ".");
