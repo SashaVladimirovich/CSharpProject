086f8ce0cf4951e7912e3a28f1b663b693d14537
Sahsa <a.v.pavlov09@gmail.com>
2022-09-23 18:51:12 +0300

    Решение Задачи №19

---
+++ HomeWork03/Example01/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+int Inversion(int number)
+{
+    int date = 0;
+    while (number != 0)
+    {
+        date = date * 10 + number % 10;
+        number = number / 10;
+    }
+    return date;
+}
+Console.WriteLine("Здравствуйте. Данная программа позволяет выводит палиндром ваше число или нет.");
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int variable = 0;
+while (Math.Abs(number) > 99999 || Math.Abs(number) < 10000)
+{
+    Console.WriteLine("Ошибка!!! Вы ввели не пятизначное число");
+    Console.Write("Введите число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+variable = Inversion(number);
+
+if (Math.Abs(number) == Math.Abs(variable))
+{
+    Console.Write("введёное вам число ЯВЛЯТСЯ палиндромом");
+}
+else
+{
+    Console.Write("введёное вам число НЕ ЯВЛЯЕТСЯ палиндромом");
+}
