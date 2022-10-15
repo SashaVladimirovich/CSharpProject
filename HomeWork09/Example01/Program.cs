43eb477994a5aea4d2c0d38a34edb3a4ae4e4439
Sahsa <a.v.pavlov09@gmail.com>
2022-10-15 10:04:03 +0300

    Решение задачи №64

---
+++ HomeWork09/Example01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+Console.Clear();
+void OutputNumber(int number)
+{
+    if (number > 0)
+    {
+        Console.Write(number + " ");
+        OutputNumber(number - 1);
+    }
+}
+int ChekNumber(int number)
+{
+    while (number < 1)
+    {
+        Console.WriteLine("Ошибка!!! Вы ввели не натуральное число");
+        Console.Clear();
+        Console.Write("Введите натуральное число N: ");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    return number;
+}
+Console.WriteLine("Здравствуйте. Данная программа выводит все натуральные числа в промежутке от N до 1");
+Console.Write("Введите натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = ChekNumber(number);
+OutputNumber(number);
